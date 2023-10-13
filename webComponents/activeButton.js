90c8f20537f989ba704c566de6b60ea5e42086eb
Jim Goodell <jgoodell2@yahoo.com>
2023-10-13 09:33:12 -0400

    Update activeButton.js

--- webComponents/activeButton.js
+++ webComponents/activeButton.js
@@ -7,7 +7,7 @@ class ActiveButton extends HTMLElement {
         super();
         // Create shaddow root
         const shadowRoot = this.attachShadow({ mode: 'open' });
-        shadowRoot.innerHTML = "<style>.button {display: inline-block; padding: 10px 20px; font-size: 16px; cursor: pointer; text-align: center; text-decoration: none; outline: none; color: #fff; background-color: #4C50AF; border: none; border-radius: 15px; box-shadow: 0 6px #999;} .button:hover {background-color: #3e418e} .button:active {  background-color: #3e418e;  box-shadow: 0 3px #666; transform: translateY(3px);}</style><button class='button'>Add to Your Content or Application</button>";
+        shadowRoot.innerHTML = "<style>.button {display: inline-block; padding: 5px 10px; font-size: 14px; cursor: pointer; text-align: center; text-decoration: none; outline: none; color: #fff; background-color: #4C50AF; border: none; border-radius: 15px; box-shadow: 0 6px #999;} .button:hover {background-color: #008CBA;} .button:active {  background-color: #008CBA;  box-shadow: 0 3px #666; transform: translateY(3px);}</style><button class='button'>Add guage to your content or application</button>";
       }
 };
 // Define webComponent as a custom element
